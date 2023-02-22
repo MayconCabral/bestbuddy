7be66afa164bb12770a570588bfe9e72750f3914
Maycon Cabral <povei@outlook.com>
2023-02-22 17:28:54 -0300

    feat ! : the application routers

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import './sass/main.scss';
-import App from './App';
+import Home from './pages/Home';
+import Gallery from './pages/Gallery';
+import Developer from './pages/Developer';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/gallery",
+    element: <Gallery />
+  },
+  {
+    path: "/developer",
+    element: <Developer />
+  },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
---
+++ src/pages/Developer.jsx
@@ -0,0 +1,5 @@
+export default function developer() {
+    return (
+        <h1>developer</h1>
+    )
+};
