5e1c401f11ea5351fd48fb96a56f5a141c0404bd
Maycon Cabral <povei@outlook.com>
2023-02-27 18:05:46 -0300

    feat ! : add datalist library / hooks to clean the code

---
+++ src/components/Input.jsx
@@ -0,0 +1,25 @@
+import DatalistInput, { startsWithValueFilter } from "react-datalist-input"
+
+export default function Input() {
+    return (
+        <div className="gallery__nav__input">
+            <DatalistInput
+                placeholder="Type a breed here."                
+                onSelect={(item) => console.log(item.value)}
+                filters={[startsWithValueFilter]}               
+                items={[
+                  { id: 'Chocolate', value: 'type a breed here' },
+                  { id: 'Coconut', value: 'Coconut' },
+                  { id: 'Mint', value: 'Mint' },
+                  { id: 'Strawberry', value: 'Strawberry' },
+                  { id: 'Vanilla', value: 'Vanilla' },
+                  { id: 'Chocolate', value: 'Chocolate' },
+                  { id: 'Coconut', value: 'Coconut' },
+                  { id: 'Mint', value: 'Mint' },
+                  { id: 'Strawberry', value: 'Strawberry' },                 
+                ]}
+            />
+            <a href='' className='gallery__nav__input-btn'>find pictures</a>
+        </div>
+    )
+};
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,13 +1,14 @@
 import  useResizeWindow  from '../hooks/useResizeWindow';
 import Cards from '../components/Cards';
 import Button from '../components/Button';
+import Input from '../components/Input';
 
 export default function Gallery() {
     const isMobile = useResizeWindow();
     
     return (
        <main className='gallery'>
-            <svg className='gallery__svg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#5594B5" fill-opacity="1" d="M0,96L48,122.7C96,149,192,203,288,229.3C384,256,480,256,576,224C672,192,768,128,864,90.7C960,53,1056,43,1152,64C1248,85,1344,139,1392,165.3L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+            <svg className='gallery__svg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#5594B5" fillOpacity="1" d="M0,96L48,122.7C96,149,192,203,288,229.3C384,256,480,256,576,224C672,192,768,128,864,90.7C960,53,1056,43,1152,64C1248,85,1344,139,1392,165.3L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
             <div className="flex-gallery">
                 <div className="gallery__container">
                    {
@@ -27,6 +28,7 @@ export default function Gallery() {
                    }
                 </div>
                 <nav className='gallery__nav'>
+                    <Input/>                  
                     <Button class='btn btn-secondary' text='home' link='/'/>
                 </nav>
             </div>
